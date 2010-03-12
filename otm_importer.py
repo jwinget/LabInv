664270ccc52ad1709e472e54bad723b7929c2873
jwinget <jwinget@gmail.com>
2010-03-12 08:53:01 -0800

    Rudimentary construct addition in place

---
+++ otm_importer.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import MySQLdb, csv, re, string
+
+otm = open('/home/jason/web/otm.csv', 'rb')
+reader = csv.reader(otm)
+
+db = MySQLdb.connect(host='localhost', user='mayorlab', passwd='T!b0m@y0r', db='labinv')
+cursor = db.cursor()
+
+reader.next()
+for row in reader:
+	oligo_name = string.upper(re.sub("\s+", "", row[0]))
+	sequence = string.lower(re.sub("\s+", "", row[1]))
+	if row[2] == '':
+		notes = 'NULL'
+	else:
+		notes = row[2]
+	supplier = row[3]
+	if row[4] == '':
+		concentration = 'NULL'
+	else:
+		concentration = row[4]
+	if row[5] == '':
+		originator = 'NULL'
+	else:
+		originator = row[5]
+	query = 'INSERT INTO oligos (id, name, sequence, supplier, concentration, date_added, originator, notes) VALUES ("NULL", "'+oligo_name+'", "'+sequence+'", "'+supplier+'", "'+concentration+'", "2010-01-06", "'+originator+'", "'+notes+'")'
+# execute SQL select statement
+	#print query
+	cursor.execute(query)
